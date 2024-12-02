7dcfc636d82a41f4307990e34e141d42bd08db21
E-Franklyn <immanfrank@gmail.com>
2024-12-02 18:57:53 -0500

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Recipe from './Recipe';
 
 const RecipeWebsite = () => {
     return (
-        <Router basename="/recipe-website">
+        <Router>
             <div>
                 {/* ------------------------------------------ Header -------------------------------------------------- */}
                 <header>
